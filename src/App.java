6858e899633ae11e6bc937ee18c8919a3b7c01b9
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-01 23:56:38 +0530

    Update App.java_Started the coding from implementing  in App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+// First Stage is to build the interface (GUI)
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      JFrame frame = new JFrame("Snake");
+        
     }
 }
