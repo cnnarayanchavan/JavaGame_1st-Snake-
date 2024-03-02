eb89ea290594f369177a8d215bc956105120b11c
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-03-02 09:02:14 +0530

    Specified the boarder of gameplay and title image

--- src/App.java
+++ src/App.java
@@ -1,18 +1,20 @@
 // First Stage is to build the interface (GUI)
 // creating a board for snake game.
+import java.awt.Color;
+
 import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-      int boardwidth = 600;
-      int boardheight = boardwidth;
       JFrame frame = new JFrame();
       frame.setTitle("Snake");
-      frame.setSize(boardwidth,boardheight);
+      //frame.setSize(boardwidth,boardheight);
+      frame.setBounds(10,10,905,700);
       frame.setVisible(true);
       frame.setResizable(false);
-      frame.setLocationRelativeTo(null);
+      // frame.setLocationRelativeTo(null);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setBackground(Color.DARK_GRAY);
       SnakeGame game = new SnakeGame();
       frame.add(game);
       
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -2,20 +2,18 @@
 //import all the neccessary packages;
 import javax.swing.JPanel;
 import java.awt.*;
-import java.awt.event.*;
-import java.util.Random;
-import javax.swing.*;
+
 
 public class SnakeGame extends JPanel{
   // public SnakeGame(int boardwidth, int boardheight){
   //   this.boardwidth= boardwidth;
   //   this.boardheight= boardheight;        
-    public SnakeGamae(){
+    public int SnakeGamae(){
       return 0;
     }
     public void paint(Graphics g){
     //setting border of title image of game
-    g.setColor(Color.WHITE);
+    g.setColor(Color.RED);
     g.drawRect(24,10,851,55);
     
   }
