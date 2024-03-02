6fe5e5cf8b519077c91d92d3ae7e34108f7bb557
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-03-02 09:21:36 +0530

    Added the first image title

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,5 +1,6 @@
 //Created a new class for inherating the properties of Jpanel class in Java.
 //import all the neccessary packages;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import java.awt.*;
 
@@ -8,17 +9,22 @@ public class SnakeGame extends JPanel{
   // public SnakeGame(int boardwidth, int boardheight){
   //   this.boardwidth= boardwidth;
   //   this.boardheight= boardheight;        
+    private ImageIcon ImageTitle;
     public int SnakeGamae(){
       return 0;
     }
     public void paint(Graphics g){
     //setting border of title image of game
+    ImageTitle = new ImageIcon("Title.jpg");
+    ImageTitle.paintIcon(this, g, 25, 11);
     g.setColor(Color.RED);
     g.drawRect(24,10,851,55);
 
     // now setting us the boarder of actual game play 
     g.setColor(Color.RED);
-    g.drawRect(24,70,851,55);  
+    g.drawRect(24,74,851,577);  
+    g.setColor(Color.BLACK);
+    g.fillRect(25, 75,850, 575);
     
   }
   }
