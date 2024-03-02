74bf0c974da6d47b54a01d6dd2ddb4720890db09
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-03-02 08:21:48 +0530

    Completed the interface

--- src/App.java
+++ src/App.java
@@ -3,7 +3,15 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-      JFrame frame = new JFrame("Snake");
-        
+      int boardwidth = 600;
+      int boardheight = boardwidth;
+      JFrame frame = new JFrame();
+      frame.setTitle("Snake");
+      frame.setSize(boardwidth,boardheight);
+      frame.setVisible(true);
+      frame.setResizable(false);
+      frame.setLocationRelativeTo(null);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
+//now we have to create the Jpanel to further game progress 
