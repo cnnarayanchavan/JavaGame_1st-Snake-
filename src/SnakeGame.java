d24e36c65248388efae038093611221b9393fc73
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-02 08:34:57 +0530

    Update SnakeGame.java_working on board of game(inherated the Jpanel class)

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,5 +1,8 @@
+//Created a new class for inherating the properties of Jpanel class in Java.
 import javax.swing.JPanel;
-
+inport java.awt.*;
 public class SnakeGame extends JPanel{
-    
+    public SnakeGname(){
+        public void paint(Graphics g);
+    }
 }
